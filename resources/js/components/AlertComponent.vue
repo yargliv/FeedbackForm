589b184cdb01fa4dd1d478fb9c4251a83cc32d9e
John Doe <johndoe@example.com>
2019-11-20 20:59:13 +0400

    Created alert component

---
+++ resources/js/components/AlertComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div v-if="showed" class="message alert" :class="type" role="alert">
+        {{ title }}
+    </div>
+</template>
+<script>
+
+import eventBus from '../eventBus';
+
+export default {
+    data() {
+        return {
+            showed: false,
+            type: null,
+            title: '',
+        }
+    },
+    mounted() {
+        eventBus.$on('SHOW_ALERT', (payload) => {
+            this.setAlert(payload);
+            this.showAlert();
+        });
+    },
+    methods: {
+        setAlert({title, type}) {
+            this.title = title;
+            this.type = type;
+        },
+        showAlert() {
+            this.showed = true;
+            setTimeout(this.hideAlert, 3000);
+        },
+        hideAlert() {
+            this.showed = false;
+        }
+    }
+
+}
+</script>
+<style scoped>
+    .message {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: auto;
+        margin-bottom: 0;
+    }
+</style>
